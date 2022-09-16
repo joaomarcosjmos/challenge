566f76386b50845beb5aa57f716cfe3ad6b2e07e
João Marcos <joaomarcosjmos.ans@gmail.com>
2022-09-15 23:53:57 -0300

    Created actor entity and done object-relational mapping

---
+++ src/main/java/com/xpand/challenge/model/Actor.java
@@ -0,0 +1,68 @@
+package com.xpand.challenge.model;
+
+import com.xpand.challenge.dto.ActorDTO;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Entity
+@Table(name = "actor")
+public class Actor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+
+    @ManyToMany(mappedBy = "actors")
+    private List<Movie> movies = new ArrayList<>();
+
+    public Actor() {
+    }
+
+    public Actor(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Actor(ActorDTO dto) {
+        this.id = dto.getId();
+        this.name = dto.getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Movie> getMovies() {
+        return movies;
+    }
+
+    public void setMovies(List<Movie> movies) {
+        this.movies = movies;
+    }
+
+    public static Actor convert(ActorDTO dto) {
+        return Optional.ofNullable(dto).map(d -> {
+            Actor actor = new Actor();
+            actor.setName(dto.getName());
+            return actor;
+        }).orElse(null);
+    }
+
+}
--- src/main/java/com/xpand/challenge/model/Movie.java
+++ src/main/java/com/xpand/challenge/model/Movie.java
@@ -1,14 +1,13 @@
 package com.xpand.challenge.model;
 
+import com.xpand.challenge.dto.MovieDTO;
+
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Entity
 @Table(name = "movie")
@@ -30,6 +29,23 @@ public class Movie {
 
     @Column(name = "revenue")
     private BigDecimal revenue;
+
+   @ManyToMany
+   @JoinTable(name = "movie_actor",
+   joinColumns = @JoinColumn(name = "movie_id"),
+   inverseJoinColumns = @JoinColumn(name = "actor_id"))
+    private List<Actor> actors = new ArrayList<>();
+
+    public Movie() {
+    }
+
+    public Movie(Long id, String title, LocalDate date, Float rank, BigDecimal revenue) {
+        this.id = id;
+        this.title = title;
+        this.date = date;
+        this.rank = rank;
+        this.revenue = revenue;
+    }
 
     public Long getId() {
         return id;
@@ -71,4 +87,22 @@ public class Movie {
         this.revenue = revenue;
     }
 
+    public List<Actor> getActors() {
+        return actors;
+    }
+
+    public void setActors(List<Actor> actors) {
+        this.actors = actors;
+    }
+
+    public static Movie convert(MovieDTO dto) {
+        return Optional.ofNullable(dto).map(d -> {
+            Movie movie = new Movie();
+            movie.setDate(dto.getDate());
+            movie.setTitle(dto.getTitle());
+            movie.setRank(dto.getRank());
+            movie.setRevenue(dto.getRevenue());
+            return movie;
+        }).orElse(null);
+    }
 }
